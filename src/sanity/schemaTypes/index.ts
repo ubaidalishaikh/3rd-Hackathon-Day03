6c53eff900370675a5fa586c06c6ed3d88985a5d
Syeda Bisma Jafri <najamkurazme@gmail.com>
2025-01-19 14:56:13 -0800

    updated

---
+++ src/sanity/schemaTypes/index.ts
@@ -0,0 +1,8 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import foods from './foods'
+import chefs from './chefs'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [foods, chefs],
+  
+}
